1b2ba60d2211ec6348ce62869bab5ad3a7cacaa4
Patrick <ammeyjohn@gmail.com>
2013-08-21 22:51:35 +0800

    增加了LoadProperty.ashx服务，用于加载lsystem保存的属性列表。
    Signed-off-by: Patrick <ammeyjohn@gmail.com>

---
+++ LSystem/src/Services/LoadProperty.ashx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace LSystem.Services
+{
+    /// <summary>
+    /// To load lsystem properties from Properties.txt.
+    /// </summary>
+    public class LoadProperty : IHttpHandler
+    {
+        public const string FILENAME = "Properties.txt";
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string properties = "";
+            var folder = context.Server.MapPath("~/Upload");
+            var fullPath = Path.Combine(folder, "Properties.txt");
+            if (File.Exists(fullPath))
+            {
+                using (StreamReader reader = new StreamReader(fullPath))
+                {
+                    properties = reader.ReadToEnd();
+                }
+            }
+
+            context.Response.ContentType = "text/json";
+            context.Response.Write(properties); 
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
